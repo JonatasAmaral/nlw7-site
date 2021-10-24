2986a1dbb8180a7dc7617930fa7a2777a8621b3f
Jonatas Amaral <jonataspadtab@gmail.com>
2021-10-24 00:03:10 -0300

    add login box

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import styles from "./App.module.scss";
+import LoginBox from "./components/LoginBox";
 
 function App() {
 	return (
 		<main className={styles.contentWrapper}>
+			<div className="MessgeList"></div>
+			<LoginBox />
 		</main>
 	);
 }
