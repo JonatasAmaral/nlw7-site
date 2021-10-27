f9c6c66606a6da4f497de1ff8aae37939ad05e8d
Jonatas Amaral <jonataspadtab@gmail.com>
2021-10-26 23:15:56 -0300

    move out signin logic to an app wide react context

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,55 +1,17 @@
-import { useEffect } from "react";
+import { useContext } from "react";
 import { VscGithubInverted } from "react-icons/vsc";
-import { api } from "../../services/api";
+import { AuthContext } from "../../contexts/auth";
 import styles from "./styles.module.scss";
 
-type AuthResponse = {
-	token: string;
-	user: {
-		id: string;
-		name: string;
-		avatar_url: string;
-		login: string;
-	};
-};
-
 export default function LoginBox() {
-	const signInURL = `https://github.com/login/oauth/authorize?scope=user&client_id=${
-		import.meta.env.VITE_GITHUB_CLIENT_ID as string
-	}&redirect_uri=http://localhost:4000/signin/callback?forward_to=${
-		window.location.href
-	}`;
+	const { signInUrl, user } = useContext(AuthContext);
 
-	async function signIn(githubCode: string) {
-		const response = await api.post<AuthResponse>("authenticate", {
-			code: githubCode,
-		});
-
-		const { token, user } = response.data;
-
-		localStorage.setItem("@dowhile:token", token);
-		console.log(user);
-	}
-
-	useEffect(() => {
-		// const queryString = window.location.search;
-		const queryString = new URLSearchParams(window.location.search);
-		const githubCode = queryString.get("code");
-
-		if (githubCode) {
-			const { origin, pathname } = window.location;
-			const cleanedURL = origin + pathname;
-
-			window.history.pushState({}, "", cleanedURL);
-
-			signIn(githubCode);
-		}
-	}, []);
+	console.log(user);
 
 	return (
 		<div className={styles.loginBoxWrapper}>
 			<strong>Entre e compartilhe sua mensagem</strong>
-			<a href={signInURL} className={styles.signWithGithub}>
+			<a href={signInUrl} className={styles.signWithGithub}>
 				<VscGithubInverted size="24" />
 				Entrar Com o Github
 			</a>
---
+++ src/contexts/auth.tsx
@@ -0,0 +1,66 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { api } from "../services/api";
+
+type User = {
+	id: string;
+	name: string;
+	avatar_url: string;
+	login: string;
+};
+type AuthContextData = {
+	user: User | null;
+	signInUrl: string;
+};
+
+type AuthResponse = {
+	token: string;
+	user: User;
+};
+
+export const AuthContext = createContext({} as AuthContextData);
+
+type AuthProvider = {
+	children: ReactNode;
+};
+
+export function AuthProvider({ children }: AuthProvider) {
+	const [user, setUser] = useState<User | null>(null);
+
+	const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=${
+		import.meta.env.VITE_GITHUB_CLIENT_ID as string
+	}&redirect_uri=http://localhost:4000/signin/callback?forward_to=${
+		window.location.href
+	}`;
+
+	async function signIn(githubCode: string) {
+		const response = await api.post<AuthResponse>("authenticate", {
+			code: githubCode,
+		});
+
+		const { token, user } = response.data;
+
+		localStorage.setItem("@dowhile:token", token);
+		setUser(user);
+	}
+
+	useEffect(() => {
+		// const queryString = window.location.search;
+		const queryString = new URLSearchParams(window.location.search);
+		const githubCode = queryString.get("code");
+
+		if (githubCode) {
+			const { origin, pathname } = window.location;
+			const cleanedUrl = origin + pathname;
+
+			window.history.pushState({}, "", cleanedUrl);
+
+			signIn(githubCode);
+		}
+	}, []);
+
+	return (
+		<AuthContext.Provider value={{ signInUrl, user }}>
+			{children}
+		</AuthContext.Provider>
+	);
+}
