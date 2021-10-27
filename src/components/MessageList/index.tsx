09bde4f260dac49de35723550c85054fe2f785ad
Jonatas Amaral <jonataspadtab@gmail.com>
2021-10-27 15:59:08 -0300

    put new websocket messages in a render delay queue

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import io from "socket.io-client";
 import { api } from "../../services/api";
 
 import styles from "./styles.module.scss";
@@ -14,9 +15,26 @@ type Message = {
 	};
 };
 
+const messagesQueue: Message[] = [];
+
+const socket = io(api.defaults.baseURL as string);
+socket.on("new_message", (newMessage: Message) => {
+	messagesQueue.push(newMessage);
+});
+
 export default function MessageList() {
 	const [messages, setMessages] = useState<Message[]>([]);
 
+	useEffect(() => {
+		const timer = setInterval(() => {
+			if (messagesQueue.length > 0) {
+				setMessages((prevMessages) =>
+					[messagesQueue[0], prevMessages[0], prevMessages[1]].filter(Boolean)
+				);
+				messagesQueue.shift();
+			}
+		}, 2000);
+	}, []);
 	useEffect(() => {
 		api.get<Message[]>("messages/last3").then((response) => {
 			setMessages(response.data);
