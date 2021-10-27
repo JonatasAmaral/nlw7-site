43a89f6013bf72d0273fc586ab565bc9d7e53746
Jonatas Amaral <jonataspadtab@gmail.com>
2021-10-27 00:10:53 -0300

    contruct form page for logged in user

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -4,9 +4,7 @@ import { AuthContext } from "../../contexts/auth";
 import styles from "./styles.module.scss";
 
 export default function LoginBox() {
-	const { signInUrl, user } = useContext(AuthContext);
-
-	console.log(user);
+	const { signInUrl } = useContext(AuthContext);
 
 	return (
 		<div className={styles.loginBoxWrapper}>
